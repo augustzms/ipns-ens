9696b94449cbc32b4d2ffdf8d1910c4d365ab244
augustzms <minsheng@glitterprotocol.io>
2022-07-24 07:42:00 +0000

    reformat the files.

--- src/dune.py
+++ src/dune.py
@@ -2,7 +2,6 @@ from duneanalytics import DuneAnalytics
 
 class Dune:
     def __init__(self, username, password):
-
         self.dune = DuneAnalytics(username, password)
         self.dune.login()
         self.dune.fetch_auth_token()
@@ -12,8 +11,6 @@ class Dune:
         # Query id for any query can be found from the url of the query:
         # For example:
         # https://duneanalytics.com/queries/4494/8769 => 4494
-        # https://duneanalytics.com/queries/3705/7192 => 3705
-        # https://duneanalytics.com/queries/3751/7276 => 3751
         result_id = self.dune.query_result_id(query_id=query_id)
         data = self.dune.query_result(result_id)
         return data
--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
+#!/usr/bin/python -tt
 import content_hash
 from dune import Dune
 import sys
 import thegraph
 
 def run():
-	# Please input the username and password.
 	dune = Dune('simplessssss', '8224074zms')
-	# For more details, please check dune_query.sql. 
 	data = dune.query_result(1062715)['data']['get_result_by_result_id']
+    f = open('../results/ens.txt', 'w')
 
 	for idx in range(len(data)):
-		# namehash is the hash from the ens name.
+		# Namehash is the hash from the ens name.
 		# For more details, refer to: https://docs.ens.domains/terminology
 		# All results in the dune are formatted as \x for hexadecimal.
 		namehash = data[idx]['data']['node'].replace('\\', '0')
@@ -18,16 +18,16 @@ def run():
 		if len(namehash) == 0 or len(contenthash) == 0:
 			continue
 
-		# Verify the codec of the contenthash, if it's from ipns, at this moment,
-		# We will dump the result.
 		try:
 			codec = content_hash.get_codec(contenthash)
-			if "ipns-ns" in codec:
+			if "ipns-ns" in codec or "ipfs-ns" in codec:
 				name = thegraph.query_ens_by_id(namehash)
 				if name is not None:
-					print(name)
+					f.write("%s\n" % name)
 		except:
 			continue
+
+    f.close()
 
 
 
